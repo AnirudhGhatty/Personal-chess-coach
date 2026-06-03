2822e279c68dbadfaa79a3ca5c60fe70ca5f3a95
Anirudh <anirudhghatty@gmail.com>
2026-06-03 19:40:56 +0530

    added data to the parser file

--- analysis/pgn_parser.py
+++ analysis/pgn_parser.py
@@ -2,4 +2,13 @@ import chess.pgn
 import io
 
 def parse_pgn(pgn_text):
-    pass
+    game = chess.pgn.read_game(io.StringIO(pgn_text))
+
+    if game == None:
+        return None
+    
+    return{
+        "white" : game.headers.get("White" , "Unknown") ,
+        "black" : game.headers.get("Black" , "Unknown") ,
+        "result" : game.headers.get("Result" , "*")
+    }
